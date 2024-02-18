517716bca5b7fd07b1aef1dc25228f3a23365d6d
Luke Trenberth <luketrenberth@gmail.com>
2024-02-18 23:52:55 +1300

    Add binary tree treversals

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,22 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // pre recurse
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post recurse
+    return path;
+
+} 
+
+
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,34 @@
-export default function quick_sort(arr: number[]): void {
+function qs(array: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    const pivot_index = partition(array, low, high)
+    qs(array, low, pivot_index-1);
+    qs(array, pivot_index + 1, high);
+}
+
+function partition(array: number[], low: number, high: number): number {
+    const pivot = array[high];
+    
+    let index = low - 1;
+
+    for (let i = low; i < high; ++i) {
+        if (array[i] <= pivot) {
+            index++;
+            const temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+    index++;
+    array[high] = array[index];
+    array[index] = pivot;
+
+    return index;
+}
+
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length-1)    
 }
